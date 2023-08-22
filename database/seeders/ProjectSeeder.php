dc33208bb5d158d2a4c4d1c138542d6bb6d3ce44
Ericson-Manalo <ericmana98@gmail.com>
2023-08-22 16:43:45 +0200

    created, by using faker my seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,14 +4,44 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $typeList = [
+            "Public",
+            "Private",
+            "Sources",
+            "Forks",
+            "Archived",
+            "Can be sponsored",
+            "Mirrors",
+            "Templates",
+        ];
+
+        $languageList = [
+            "PHP",
+            "Hack",
+            "Vue",
+            "CSS",
+            "HTML",
+            "Javascript",
+        ];
+
+        
+        for ($i=0; $i < 50; $i++) {
+            $newProject = new Project();
+            $newProject->title = $faker->unique()->sentence(3);
+            $newProject->description = $faker->paragraph(7);
+            $newProject->type = $faker->randomElement($typeList);
+            $newProject->language = $faker->randomElement($languageList);
+            $newProject->save();
+            # code...
+        }
     }
 }
