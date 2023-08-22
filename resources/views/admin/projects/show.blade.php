24d9c542ffaf6e7738dfa9d1a6596d418871cb03
Ericson-Manalo <ericmana98@gmail.com>
2023-08-22 18:45:40 +0200

    show the single card when view is clicked

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="col-7 m-auto mt-5">
+    <div class="card text-center">
+        <div class="card-header">
+            #{{$project->id}} - {{$project->title}}
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">
+                {{$project->title}}
+            </h5>
+            <p class="card-text">
+                {{$project->description}}
+            </p>
+            <p class="card-text">
+                {{$project->type}} - {{$project->language}}
+            </p>
+            <div class="card-footer text-muted">
+                {{$project->created_date}}
+            </div>
+            
+        </div>
+        <div class="card-footer text-muted">
+            <a href="" class="badge bg-success p-2 m-1">Edit</a>
+            <a href="" class="badge bg-danger p-2 m-1">Delete</a>
+        </div>
+    </div>
+</div>
+@endsection
