9dba00e3a48dcd775fa7e91095fac3cc28c84798
Ericson-Manalo <ericmana98@gmail.com>
2023-08-23 18:37:06 +0200

    made a test using dd, if by clicking the button it works

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -103,8 +103,9 @@ class ProjectController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Project $project)
     {
         //
+        dd($project);
     }
 }
