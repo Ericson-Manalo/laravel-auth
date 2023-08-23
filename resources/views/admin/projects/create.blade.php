9282e827d42b4d6a7f60c720254fc23fad3acc9d
Ericson-Manalo <ericmana98@gmail.com>
2023-08-23 16:38:29 +0200

    added the error that shows when data's are inserted as requested

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -4,10 +4,19 @@
 <div class="col-7 m-auto mt-5">
     <form action="{{ route('admin.projects.store') }}" method="POST">
                 @csrf
+
+
+    @error('title')
+        <div class="error">{{ $message }}</div>
+    @enderror            
     <div class="mb-3">
         <label for="title" class="form-label">Title Project</label>
         <input type="text" class="form-control" id="title" name="title">
     </div>
+
+    @error('description')
+        <div class="error">{{ $message }}</div>
+    @enderror     
     <div class="mb-3">
         <label for="description" class="form-label">Description</label>
         <textarea name="description" id="description" cols="95 "rows="10"></textarea>
@@ -39,11 +48,11 @@
         <input type="text" class="form-control" id="language" name="language">
     </div> -->
     <div class="form-group">
-        <label for="type" class="form-label">Type</label>
+        <label for="language" class="form-label">Language</label>
             <div class="col-md-10 inputGroupContainer">
                 <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-file-code-o"></i></span>
-                    <select class="form-control" id="type">
+                    <select class="form-control" id="language">
                         <option>PHP</option>
                         <option>Hack</option>
                         <option>Vue</option>
